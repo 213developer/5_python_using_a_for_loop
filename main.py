c6ff0d07159b35d5f808ac40ba44b3caceba9eab
“javascriptseo” <seofreelance2021@outlook.com>
2023-09-28 19:08:55 -0700

    completed for loop

--- main.py
+++ main.py
@@ -13,3 +13,7 @@ NUM_LOOP_END = 10 # Constant used to control loop
 print("0 through 10 multiplied by 2 and by 10.")
 
 # Write your for loop here
+for number in range(NUM_LOOP_START, NUM_LOOP_END):
+    print(head1 + str(number))
+    print(head2 + str(number * 2))
+    print(head3 + str(number * 10))
